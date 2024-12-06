ad62f1805ecbdc12180003544185e3d6ea31beeb
Karan0207 <minidox0207@gmail.com>
2024-12-06 11:17:00 +0530

    drizzle config setup

--- src/utils/db/dbConfig.jsx
+++ src/utils/db/dbConfig.jsx
@@ -1,9 +1,7 @@
-import {neon} from "@neondatabase/serverless";
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from './schema';
 
-import {drizzle} from "drizzle-orm/neon-http"
-import * as schema from "./schema.ts"
-import { dirxml } from "console";
+const sql = neon(process.env.DATABASE_SQL);
 
-const sql = neon(process.env.DATABASE_URL);
-
-export const db= drizzle(sql,{schema})
+export const db = drizzle(sql, { schema });
